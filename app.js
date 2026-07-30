3938b4d97190a4c8b0910c9757a63e5173e56d54
Vinícius Juliani Pereira <34162734+viijuliani@users.noreply.github.com>
2026-07-30 14:52:11 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const API_URL =
-    "https://script.google.com/macros/s/AKfycbzXtx1hj2pzACB5cn7MWlBPs6CjTfidVI5bK0-MQNb-c2g_dKo_3uZmGZqNLkrMTVyH/exec";
+    "https://script.google.com/macros/s/AKfycbxyuIV5Z_4iSWnj_JM2dKLq6FW5U4glq5mSRXa3CQLy6JFjQDuXYUoxmFXyL06_x1WI/exec";
 
 const pathways = [
     "Optimise Flexibility",
