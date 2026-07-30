3345d382fdd36a04f647224d81d23a63f8a3b0e8
Vinícius Juliani Pereira <34162734+viijuliani@users.noreply.github.com>
2026-07-30 12:57:23 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,12 +1,9 @@
 window.onload = function () {
 
-    console.log("noUiSlider =", noUiSlider);
-
     const survey = document.getElementById("survey");
 
     survey.innerHTML = `
-        <h2>Test</h2>
-        <p>If you can see this, app.js is running.</p>
+        <h2>Everything works!</h2>
     `;
 
 };
