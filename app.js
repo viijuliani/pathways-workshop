d83f6de4ba0e89e682b25b670f733b539610ead0
Vinícius Juliani Pereira <34162734+viijuliani@users.noreply.github.com>
2026-07-30 10:12:33 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,42 +1,4 @@
-const API_URL =
-"https://script.google.com/macros/s/AKfycbzXtx1hj2pzACB5cn7MWlBPs6CjTfidVI5bK0-MQNb-c2g_dKo_3uZmGZqNLkrMTVyH/exec";
-
-fetch(API_URL)
-.then(response => response.json())
-.then(data => {
-
-  const container =
-  document.getElementById("survey");
-
-  data.criteria.forEach(item => {
-
-    const div =
-    document.createElement("div");
-
-    div.innerHTML = `
-      <h3>${item}</h3>
-      <input
-      type="range"
-      min="0"
-      max="100"
-      step="5">
-    `;
-
-    container.appendChild(div);
-
-  });
-
-});
-const pathways = [
-
-"Optimise Flexibility",
-
-"Monetise Flexibility",
-
-"Collectivise Flexibility",
-
-"Democratic Flexibility",
-
-"Alternative to Flexibility"
-
-];
+document.getElementById("survey").innerHTML = `
+    <h2>Success!</h2>
+    <p>Your JavaScript is loading correctly.</p>
+`;
