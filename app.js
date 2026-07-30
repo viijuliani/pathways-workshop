6796f50a3843c1ae6ce994d712d36a5ce8ebac37
Vinícius Juliani Pereira <34162734+viijuliani@users.noreply.github.com>
2026-07-30 13:38:29 +0100

    Update app.js

--- app.js
+++ app.js
@@ -3,10 +3,47 @@ window.onload = function () {
     const survey = document.getElementById("survey");
 
     survey.innerHTML = `
-        <h2>Slider Library Test</h2>
-        <p>Check the browser console.</p>
+        <h2>Slider Test</h2>
+
+        <div id="slider"></div>
+
+        <div class="scale-labels">
+            <span>0</span>
+            <span>50</span>
+            <span>100</span>
+        </div>
+
+        <p>
+            Min:
+            <span id="minValue">20</span>
+
+            &nbsp;&nbsp;&nbsp;
+
+            Max:
+            <span id="maxValue">80</span>
+        </p>
     `;
 
-    console.log("noUiSlider =", typeof noUiSlider);
+    const slider = document.getElementById("slider");
+
+    noUiSlider.create(slider, {
+        start: [20, 80],
+        connect: true,
+        step: 5,
+        range: {
+            min: 0,
+            max: 100
+        }
+    });
+
+    slider.noUiSlider.on("update", function (values) {
+
+        document.getElementById("minValue").textContent =
+            Math.round(values[0]);
+
+        document.getElementById("maxValue").textContent =
+            Math.round(values[1]);
+
+    });
 
 };
