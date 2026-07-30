d4a6987cb1399f95e3ee4ab1d56781018b6a3837
Vinícius Juliani Pereira <34162734+viijuliani@users.noreply.github.com>
2026-07-30 13:17:01 +0100

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,10 @@ window.onload = function () {
     const survey = document.getElementById("survey");
 
     survey.innerHTML = `
-        <h2>Everything works!</h2>
+        <h2>Slider Library Test</h2>
+        <p>Check the browser console.</p>
     `;
+
+    console.log("noUiSlider =", typeof noUiSlider);
 
 };
